5a6fb898fafac43964f935c7c74c6963c7224b74
Chris Kuzma <chris.kuzma@gmail.com>
2018-05-23 17:07:51 -0700

    Updated with webhook working now

--- server.py
+++ server.py
@@ -1,12 +1,6 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 
-## Import and instantiate custom stuff
-from config import Config
-from utils.timestamp import Timestamp
-config = Config()
-timestamp = Timestamp()
-
 ## Instantiate the Flask stuff
 app = Flask(__name__)
 api = Api(app)
@@ -18,27 +12,39 @@ REPLY_CONTENTS = {
             'lastDoorMeasurement': None,
             'lastDoorChange': None,
             'lastInsideChange': None,
-            'lastInsideValue': None
         }
 
 class Main(Resource):
-    def __init__(self):
-        pass
-
     def get(self):
         return REPLY_CONTENTS
 
 class Update(Resource):
     def __init__(self):
-        parser.add_argument('name')
+        parser.add_argument('event')
         parser.add_argument('data')
-        parser.add_argument('coreid')
         parser.add_argument('published_at')
+        parser.add_argument('coreid')
+        pass
 
     def post(self):
-        args = parser.parse_args(strict=True)
+        args = parser.parse_args()
         print(args)
+        self.update_status(args)
         return 'OK'
+
+    def update_status(self, args):
+        if args['event'] == 'distance-inch':
+            door_open = None
+            if float(args['data']) > 4.0:
+                door_open = True
+            else:
+                door_open = False
+            if REPLY_CONTENTS['doorOpen'] != door_open:
+                REPLY_CONTENTS['lastDoorChange'] = args['published_at']
+            REPLY_CONTENTS['doorOpen'] = door_open
+            REPLY_CONTENTS['lastDoorMeasurement'] = args['published_at']
+        if args['event'] == 'pir-sensor':
+            REPLY_CONTENTS['lastInsideChange'] = args['published_at']
         
 api.add_resource(Update, '/update')
 api.add_resource(Main, '/')
